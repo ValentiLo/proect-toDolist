71608cfd8b86c95a05033a7a163fecaab9ce6402
ValentiLo <125864669+ValentiLo@users.noreply.github.com>
2025-05-28 14:05:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-// let todoForm = document.querySelector('.todo-form');
-// let commentList = document.querySelector('.todo-list');
-// let todoInput = document.querySelector('.todo-input');
-// let priority = document.querySelector('.todo-priority');
-
-// priority.onclick = function () {
-// if (priority.classList.contains('is-important')) {
-// priority.textContent = 'Обычная задача';
-// } else {
-// priority.textContent = 'Важная задача';
-// }
-// priority.classList.toggle('is-important');
-// };
-
-// todoForm.onsubmit = function (evt) {
-//   evt.preventDefault();
-
-//   let newComment = document.createElement('li');
-//   if (priority.classList.contains('is-important')) {
-// newElement.classList.add('is-important')
-// };
-//   newComment.classList.add('todo-list');
-//   newComment.textContent = todoInput.value;
-//   todoInput.value = '';
-//   commentList.append(newComment);
-// };
 let list = document.querySelector('.todo-list');
 let input = document.querySelector('.todo-input');
 let form = document.querySelector('.todo-form');
@@ -40,24 +14,6 @@ priority.onclick = function () {
 
 form.onsubmit = function (evt) {
   evt.preventDefault();
-};
-
-let list = document.querySelector('.todo-list');
-let input = document.querySelector('.todo-input');
-let form = document.querySelector('.todo-form');
-let priority = document.querySelector('.todo-priority');
-
-priority.onclick = function () {
-if (priority.classList.contains('is-important')) {
-priority.textContent = 'Обычная задача';
-} else {
-priority.textContent = 'Важная задача';
-}
-priority.classList.toggle('is-important');
-};
-
-form.onsubmit = function (evt) {
-evt.preventDefault();
 
 let newElement = document.createElement('li');
 if (priority.classList.contains('is-important')) {
